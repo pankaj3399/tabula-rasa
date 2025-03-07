c2058f65d9ffa746e4ca9e0fdbc68f9eb3391527
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-03-07 20:18:26 +0530

    Add HowItWorks component to Home page; update Navbar styles and links

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import HeroSection from '../components/HeroSection';
 import FeatureSection from '../components/FeatureSection';
 import TestimonialSection from '../components/TestimonialSection';
 import CTASection from '../components/CTASection';
+import HowItWorks from '../components/HowItWorks';
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 to-purple-100">
       <div className="container mx-auto px-4">
         <HeroSection />
+        <HowItWorks />
         <FeatureSection />
         <TestimonialSection />
         <CTASection />
