6ec0d2763f02cddd235152613da645be6fc0ac01
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-03-20 17:59:08 +0530

    added changes

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: '@strapi/provider-upload-cloudinary', // Use the correct provider name
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
